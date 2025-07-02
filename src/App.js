66608107aaa49a9f2c1319ad502c5677f5f5be85
Aashutosh Marathe <93965247+aashu29jan@users.noreply.github.com>
2025-07-02 15:23:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'; // Importing the CSS file for styles
 const App = () => {
   return (
     <div className="container">
-      <h1 className="text">First Codepipeline creation successful.</h1>
+      <h1 className="text">First Codepipeline creation successful, Code Pipeline Tested </h1>
     </div>
   );
 };
